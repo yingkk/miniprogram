477d529718441e6637e74c13a11983c6fd02a51b
Carol <oncwnuDS7X6h9LpPxLZ0VrVKcPkU@git.weixin.qq.com>
2020-04-10 09:37:18 +0800

    commit

---
+++ pages/index/index.js
@@ -0,0 +1,200 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    activedTag: "动物世界",
+    selectedList: [],
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    swiperList: [{
+        url: "../../images/swiper1.jpeg",
+        label: "paint"
+      },
+      {
+        url: "../../images/swiper2.jpeg",
+        label: "nature"
+      },
+      {
+        url: "../../images/swiper3.jpg",
+        label: "travel"
+      },
+      {
+        url: "../../images/swiper4.jpg",
+        label: "family"
+      }
+    ],
+    hotList: [{
+        url: "../../images/hot1.jpg",
+        label: "野餐时光"
+      },
+      {
+        url: "../../images/hot2.jpg",
+        label: "镜中万物"
+      },
+      {
+        url: "../../images/hot3.jpg",
+        label: "云惊四意"
+      },
+      {
+        url: "../../images/hot4.jpg",
+        label: "欢乐亲子"
+      }
+    ],
+    areaList: [{
+        url: "../../images/area1.jpg",
+        label: "大象宝宝"
+      },
+      {
+        url: "../../images/area2.jpg",
+        label: "牛大厨"
+      },
+      {
+        url: "../../images/area3.jpg",
+        label: "小蓝狗"
+      },
+      {
+        url: "../../images/area4.jpeg",
+        label: "喵咪"
+      },
+      {
+        url: "../../images/area5.jpg",
+        label: "毛驴儿"
+      },
+      {
+        url: "../../images/area6.jpeg",
+        label: "樱花树"
+      },
+      {
+        url: "../../images/area7.jpg",
+        label: "红枫叶"
+      },
+      {
+        url: "../../images/area8.jpg",
+        label: "苹果树"
+      },
+      {
+        url: "../../images/area9.jpg",
+        label: "椰子树"
+      },
+      {
+        url: "../../images/area10.jpg",
+        label: "竹子"
+      }
+    ],
+    likeList: [{
+        tag: "动物世界",
+        itemList: [{
+            url: "../../images/monkey.jpg",
+            name: "猴子",
+            eats: ["香蕉", "苹果", "蛋糕"],
+            activity: "爬树",
+            address: "野生动物馆"
+          },
+          {
+            url: "../../images/monkey.jpg",
+            name: "猴子",
+            eats: ["香蕉", "苹果", "蛋糕"],
+            activity: "爬树",
+            address: "野生动物馆"
+          },
+          {
+            url: "../../images/monkey.jpg",
+            name: "猴子",
+            eats: ["香蕉", "苹果", "蛋糕"],
+            activity: "爬树",
+            address: "野生动物馆"
+          }
+        ]
+      },
+      {
+        tag: "植物王国",
+        itemList: [{
+            url: "../../images/tree.jpeg",
+            name: "松树",
+            eats: ["浇水", "施肥"],
+            activity: "落叶",
+            address: "植物馆"
+          },
+          {
+            url: "../../images/tree.jpeg",
+            name: "松树",
+            eats: ["浇水", "施肥"],
+            activity: "落叶",
+            address: "植物馆"
+          },
+          {
+            url: "../../images/tree.jpeg",
+            name: "松树",
+            eats: ["浇水", "施肥"],
+            activity: "落叶",
+            address: "植物馆"
+          }
+        ]
+      }
+    ]
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad() {
+    const _this = this;
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true,
+        selectedList: _this.data.likeList[0]["itemList"]
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true,
+          selectedList: _this.data.likeList[0]["itemList"]
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true,
+            selectedList: _this.data.likeList[0]["itemList"]
+          })
+        }
+      })
+    }
+  },
+  getUserInfo(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  chooseLike(e) {
+    this.setData({
+      activedTag: e.currentTarget.dataset.tag,
+      selectedList: e.currentTarget.dataset.detail
+    })
+  },
+  swiperClick(e) {
+    const currentLabel = e.currentTarget.dataset.label;
+    wx.navigateTo({
+      url: "../main/index?currentLabel=" + currentLabel,
+    })
+  },
+  scroll(e) {
+    console.log("scroll");
+  }
+})
---
+++ pages/main/index.js
@@ -0,0 +1,112 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    currentLabel: "",
+    currentDetail: {},
+    mainDetail: [{
+        label: "paint",
+        title: "写生",
+        description: "笔下的春天依旧春意盎然",
+        address: "公园",
+        tips: "提供画板支架",
+        detail: {
+          url: "../../images/swiper1.jpeg",
+          text: "春意"
+        }
+      },
+      {
+        label: "nature",
+        title: "自然",
+        description: "带你领略不一样的自然风光",
+        address: "花园",
+        tips: "提供画板支架",
+        detail: {
+          url: "../../images/swiper1.jpeg",
+          text: "春意"
+        }
+      }, {
+        label: "travel",
+        title: "郊游",
+        description: "吃喝玩乐说走就走",
+        address: "野外",
+        tips: "",
+        detail: {
+          url: "../../images/swiper1.jpeg",
+          text: "春意"
+        }
+      }, {
+        label: "family",
+        title: "亲子",
+        description: "一家人的快乐时光",
+        address: "公园",
+        tips: "提供午餐",
+        detail: {
+          url: "../../images/swiper1.jpeg",
+          text: "春意"
+        }
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(option) {
+    this.setData({
+      currentLabel: option.currentLabel,
+      currentDetail: this.data.mainDetail.filter(item => item.label === option.currentLabel)[0]
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
